032fd0e6f06ff0a76bb91c3bc41bf63379716317
Christopher Maher <christopher.maher@alaskaair.com>
2016-05-06 09:59:26 -0700

    Added C# file for first push

---
+++ CookieJar.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security;
+using System.Security.Principal;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+
+namespace TestTools
+{
+    public class CookieJar: 
+        IDisposable
+    {
+        public CookieJar(string username, string domainname, string password)
+        {
+            ImpersonateUser(username, domainname, password);
+        }
+        public void Dispose()
+        {
+            UndoImpersonation();
+        }
+        #region DLLImports,
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern int LogonUser(
+            string lpszUserName,
+            string lpszDomain,
+            string lpszPassword,
+            int dwLogonType,
+            int dwLogonProvider,
+            ref IntPtr phToken);
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int DuplicateToken(
+            IntPtr hToken,
+            int impersonationLevel,
+            ref IntPtr hNewToken);
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool RevertToSelf();
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        private static extern bool CloseHandle(
+            IntPtr handle);
+
+        private const int LOGON32_LOGON_INTERACTIVE = 2;
+        private const int LOGON32_PROVIDER_DEFAULT = 0;
+        #endregion
+        private void ImpersonateUser(string username, string domainname, string password)
+        {
+            WindowsIdentity tempIdentity = null;
+            IntPtr token = IntPtr.Zero;
+            IntPtr tokenDuplicate = IntPtr.Zero;
+            try
+            {
+                if (RevertToSelf())
+                {
+                    if (LogonUser(username, domainname, password, LOGON32_LOGON_INTERACTIVE, LOGON32_PROVIDER_DEFAULT, ref token) != 0)
+                    {
+                        if (DuplicateToken(token, 2, ref tokenDuplicate) != 0)
+                        {
+                            tempIdentity = new WindowsIdentity(tokenDuplicate);
+                            impersonationContext = tempIdentity.Impersonate();
+                        }
+                        else
+                        {
+                            throw new Win32Exception(Marshal.GetLastWin32Error());
+                        }
+                    }
+
+                    else
+                    {
+                        throw new Win32Exception(Marshal.GetLastWin32Error());
+                    }
+                }
+                else
+                {
+                    throw new Win32Exception(Marshal.GetLastWin32Error());
+                }
+
+            }
+            finally
+            {
+                if (token != IntPtr.Zero)
+                {
+                    CloseHandle(token);
+                }
+                if (tokenDuplicate != IntPtr.Zero)
+                {
+                    CloseHandle(tokenDuplicate);
+                }
+            }
+
+        }
+        private void UndoImpersonation()
+        {
+            if(impersonationContext != null)
+            {
+                impersonationContext.Undo();
+            }
+        }
+        private WindowsImpersonationContext impersonationContext = null;
+    }
+   
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestTools;
+
+namespace CSImpersonator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using(new 
+            Console.WriteLine("Logged in User: " + Environment.UserName.ToString());
+            using(new CookieJar("username","domain","password"))
+            {
+                Console.WriteLine("Logged in user: " + System.Environment.UserName.ToString());
+                
+            }
+            Console.WriteLine("Logged in User:" + Environment.UserName.ToString());
+        }
+    }
+}
